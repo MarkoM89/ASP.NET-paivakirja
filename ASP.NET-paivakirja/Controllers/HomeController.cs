65ac590b9c52f799064d754ebc13d651878c115c
Marko Mustonen <marko.mustonen3@metropolia.fi>
2025-01-30 15:43:17 +0200

    Poistettu Privacy-sivu - aloitettu sisäänkirjautumis-sivun teko

--- ASP.NET-paivakirja/Controllers/HomeController.cs
+++ ASP.NET-paivakirja/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult Kirjaudu_Luo()
     {
         return View();
     }
